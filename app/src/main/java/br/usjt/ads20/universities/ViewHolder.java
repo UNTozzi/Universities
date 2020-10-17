f1b5c31d27c73537a529b5f674e81f453117f220
tozzi <pedrogbtozzi@gmail.com>
2020-10-16 22:26:05 -0300

    Entregando a prova

---
+++ app/src/main/java/br/usjt/ads20/universities/ViewHolder.java
@@ -0,0 +1,55 @@
+package br.usjt.ads20.universities;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Nome: Pedro Gabriel Bezerra Tozzi
+ * RA: 818229341
+ */
+
+public class ViewHolder {
+    private ImageView universityPoster;
+    private TextView universityName, universityCountry, universityWebPage;
+
+    public ViewHolder(ImageView universityPoster, TextView universityName, TextView universityCountry, TextView universityWebPage) {
+        this.universityPoster = universityPoster;
+        this.universityName = universityName;
+        this.universityWebPage = universityWebPage;
+        this.universityCountry = universityCountry;
+
+    }
+
+    public ImageView getUniversityPoster() {
+        return universityPoster;
+    }
+
+    public void setUniversityPoster(ImageView universityPoster) {
+        this.universityPoster = universityPoster;
+    }
+
+    public TextView getUniversityCountry() {
+        return universityCountry;
+    }
+
+    public void setUniversityCountry(TextView universityCountry) {
+        this.universityCountry = universityCountry;
+    }
+
+    public TextView getUniversityWebPage() {
+        return universityWebPage;
+    }
+
+    public void setUniversityWebPage(TextView universityWebPage) {
+        this.universityWebPage = universityWebPage;
+    }
+
+    public TextView getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(TextView universityName) {
+        this.universityName = universityName;
+    }
+
+}
---
+++ app/src/main/java/br/usjt/ads20/universities/universityRow.java
@@ -0,0 +1,19 @@
+package br.usjt.ads20.universities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Nome: Pedro Gabriel Bezerra Tozzi
+ * RA: 818229341
+ */
+
+public class universityRow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_university_row);
+    }
+}
